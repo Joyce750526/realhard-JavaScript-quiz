9b191eee307e4658dfabaa87d1db0b7f18af9129
Joyce Chen <joyceideas@outlook.com>
2022-07-31 05:20:04 -0400

    rewriting js codes

--- assets/script.js
+++ assets/script.js
@@ -7,131 +7,118 @@ function startGame() {
   BtnNext.style.display = "block";
   var BtnPrevious = document.getElementById("previous");
   BtnPrevious.style.display = "block";
+  builtQuiz();
 }
 
-
-
 // Variables
-var quizContainer = document.getElementById('start-button');
-var BtnNext = document.getElementById('next');
-var BtnPrevious = document.getElementById('previous');
-var BtnSubmit = document.getElementById('submit-button');
+var timeleft = 75;
+var quizContainer = document.getElementById("start-button");
+var BtnNext = document.getElementById("next");
+var BtnPrevious = document.getElementById("previous");
+var BtnSubmit = document.getElementById("submit-button");
 var myQuestions = [
   {
-    question: "Who invented JavaScript?",
+    question: "What is it called when you turn elements into a list?",
     answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich"
+      choice1: "Function",
+      choice2: "Array",
+      choice3: "Loop",
+      choice4: "String",
+      answer: 2,
     },
-    correctAnswer: "c"
   },
   {
     question: "Which one of these is a JavaScript package manager?",
     answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm"
+      choice1: "Node.js",
+      choice2: "TypeScript",
+      choice3: "npm",
+      answer: 3,
     },
-    correctAnswer: "c"
   },
   {
     question: "Which tool can you use to ensure code quality?",
     answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint"
+      choice1: "Angular",
+      choice2: "jQuery",
+      choice3: "RequireJS",
+      choice4: "ESLint",
+      answer: 4,
     },
-    correctAnswer: "d"
-  }
+  },
 ];
 
+(function () {
+  // Functions
+  function buildQuiz() {
+    // variable to store the HTML output
+    const output = [];
 
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // variable to store the list of possible answers
+      const answers = [];
 
-
-
-
-
-
-(function(){
-    // Functions
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
-  
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
                 <input type="radio" name="question${questionNumber}" value="${letter}">
                 ${letter} :
                 ${currentQuestion.answers[letter]}
               </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="slide">
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="slide">
               <div class="question"> ${currentQuestion.question} </div>
               <div class="answers"> ${answers.join("")} </div>
             </div>`
-          );
-        }
       );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');
-    }
-  
-    function showResults(){
-  
-      // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
-        }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
+    });
 
-    // Event listeners
-    submitButton.addEventListener('click', showResults);
-    previousButton.addEventListener("click", showPreviousSlide);
-    nextButton.addEventListener("click", showNextSlide);
-  })();
-  
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      }
+      // if answer is wrong or blank
+      else {
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  // Event listeners
+  submitButton.addEventListener("click", showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
