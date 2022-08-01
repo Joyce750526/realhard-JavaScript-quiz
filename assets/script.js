695c552e7e56afa14beb3d0058d22a13b6231071
Joyce Chen <joyceideas@outlook.com>
2022-08-01 03:23:05 -0400

    rewriting codes

--- assets/script.js
+++ assets/script.js
@@ -1,146 +1,149 @@
 // Variables
 var BtnStart = document.getElementById("start-button");
 var quizInstruction = document.querySelector(".quiz-insturction");
-var timeleft = 75;
 var quizContainer = document.querySelector("#quiz-container");
 var BtnNext = document.getElementById("next");
-var questionText= document.createElement("h1");
+var questionText = document.createElement("h1");
 var BtnPrevious = document.getElementById("previous");
 var BtnSubmit = document.getElementById("submit-button");
-var option1 = document.createElement("button");
-var option2 = document.createElement("button");
-var option3 = document.createElement("button");
-var option4 = document.createElement("button");
+var timeCount=document.querySelector("timer", "seconds-counter");
+// var option1 = document.createElement("button");
+// var option2 = document.createElement("button");
+// var option3 = document.createElement("button");
+// var option4 = document.createElement("button");
+
+// var timeleft = 75;
+// var interval = setInterval(function(){
+//     document.getElementById('timeleft').innerHTML=count;
+//     count--;
+//     if (count === 0){
+//       clearInterval(interval);
+//       document.getElementById('timeleft').innerHTML='Done';
+//       // or...
+//       alert("You're out of time!");
+//     }
+//   }, 1000);
+
+
 var myQuestions = [
-  {
-    question: "What is it called when you turn elements into a list?",
-    answers: {
-      choice1: "Function",
-      choice2: "Array",
-      choice3: "Loop",
-      choice4: "String",
-      answer: 2,
+    {
+        question: "What is it called when you turn elements into a list?",
+        answers: {
+            choice1: "Function",
+            choice2: "Array",
+            choice3: "Loop",
+            choice4: "String",
+            answer: 2,
+        },
     },
-  },
-  {
-    question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      choice1: "Node.js",
-      choice2: "TypeScript",
-      choice3: "npm",
-      answer: 3,
+    {
+        question: "Which one of these is a JavaScript package manager?",
+        answers: {
+            choice1: "Node.js",
+            choice2: "TypeScript",
+            choice3: "npm",
+            answer: 3,
+        },
     },
-  },
-  {
-    question: "Which tool can you use to ensure code quality?",
-    answers: {
-      choice1: "Angular",
-      choice2: "jQuery",
-      choice3: "RequireJS",
-      choice4: "ESLint",
-      answer: 4,
+    {
+        question: "Which tool can you use to ensure code quality?",
+        answers: {
+            choice1: "Angular",
+            choice2: "jQuery",
+            choice3: "RequireJS",
+            choice4: "ESLint",
+            answer: 4,
+        },
     },
-  },
 ];
 
 // Functions
 function startGame() {
-  quizInstruction.classList.add("hide");
-  quizContainer.classList.remove("hide");
-  BtnStart.style.display = "none";
-  var BtnNext = document.getElementById("next");
-  BtnNext.style.display = "block";
-  var BtnPrevious = document.getElementById("previous");
-//   BtnPrevious.style.display = "block";
-  BtnPrevious.classList.add("hide");
-  builtQuiz();
+    quizInstruction.classList.add("hide");
+    quizContainer.classList.remove("hide");
+    BtnStart.style.display = "none";
+    var BtnNext = document.getElementById("next");
+    BtnNext.style.display = "block";
+    var BtnPrevious = document.getElementById("previous");
+    //   BtnPrevious.style.display = "block";
+    BtnPrevious.classList.add("hide");
+    builtQuiz();
+    
 }
 
 function builtQuiz() {
-  option1.innerHTML = myQuestions[0].answers.choice1;
-  option2.innerHTML = myQuestions[0].answers.choice2;
-  option3.innerHTML = myQuestions[0].answers.choice3;
-  option4.innerHTML = myQuestions[0].answers.choice4;
-  question1.innerHTML = text[0].answers.choice1;
+    option1.innerHTML = myQuestions[0].answers.choice1;
+    option2.innerHTML = myQuestions[0].answers.choice2;
+    option3.innerHTML = myQuestions[0].answers.choice3;
+    option4.innerHTML = myQuestions[0].answers.choice4;
+    question1.innerHTML = text[0].answers.choice1;
+
+    quizContainer.appendChild(option1);
+    quizContainer.appendChild(option2);
+    quizContainer.appendChild(option3);
+    quizContainer.appendChild(option4);
 
 
-  quizContainer.appendChild(option1);
-  quizContainer.appendChild(option2);
-  quizContainer.appendChild(option3);
-  quizContainer.appendChild(option4);
-
-
-
-
-
+element.textContent=""
 
 
 
+    // variable to store the HTML output
+    const output = [];
 
-  // variable to store the HTML output
-  const output = [];
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        // variable to store the list of possible answers
+        const answers = [];
 
-  // for each question...
-  myQuestions.forEach((currentQuestion, questionNumber) => {
-    // variable to store the list of possible answers
-    const answers = [];
-
-    // and for each available answer...
-    for (letter in currentQuestion.answers) {
-      // ...add an HTML radio button
-      answers.push(
-        `<label>
+        // and for each available answer...
+        for (letter in currentQuestion.answers) {
+            // ...add an HTML radio button
+            answers.push(
+                `<label>
                 <input type="radio" name="question${questionNumber}" value="${letter}">
                 ${letter} :
                 ${currentQuestion.answers[letter]}
               </label>`
-      );
-    }
+            );
+        }
 
-    // add this question and its answers to the output
-    output.push(
-      `<div class="slide">
+        // add this question and its answers to the output
+        output.push(
+            `<div class="slide">
               <div class="question"> ${currentQuestion.question} </div>
               <div class="answers"> ${answers.join("")} </div>
             </div>`
-    );
-  });
+        );
+    });
 
-  // finally combine our output list into one string of HTML and put it on the page
-//   quizContainer.innerHTML = output.join("");
+    // finally combine our output list into one string of HTML and put it on the page
+    //   quizContainer.innerHTML = output.join("");
 }
 
 function showResults() {
-  // gather answer containers from our quiz
-  const answerContainers = quizContainer.querySelectorAll(".answers");
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-  // keep track of user's answers
-  let numCorrect = 0;
+    // keep track of user's answers
+    let numCorrect = 0;
 
-  // for each question...
-  myQuestions.forEach((currentQuestion, questionNumber) => {
-    // find selected answer
-    const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-    // if answer is correct
-    if (userAnswer === currentQuestion.correctAnswer) {
-      // add to the number of correct answers
-      numCorrect++;
+        // if answer is correct
+        if (userAnswer === currentQuestion.correctAnswer) {
+            // add to the number of correct answers
+            numCorrect++;
+        }
+    });
 
-      // color the answers green
-      answerContainers[questionNumber].style.color = "lightgreen";
-    }
-    // if answer is wrong or blank
-    else {
-      // color the answers red
-      answerContainers[questionNumber].style.color = "red";
-    }
-  });
-
-  // show number of correct answers out of total
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
 // // Event listeners
